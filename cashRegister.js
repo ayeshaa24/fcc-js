4b3dd715989d46d3fb542949dffaa9575a5aac93
Ayesha <ayeshakhtar24@gmail.com>
2020-08-05 22:54:41 +0100

    updated

--- cashRegister.js
+++ cashRegister.js
@@ -3,32 +3,37 @@ function checkCashRegister(price, cash, cid) {
   let diff = (cash - price) * 100;
 
   if (original === diff) {
-    return { status:"CLOSED", change:cid }
+    return { status: "CLOSED", change: cid }
   }
 
   if (original < diff) {
-    return { status:"INSUFFICIENT_FUNDS", change:[] }
+    return { status: "INSUFFICIENT_FUNDS", change: [] }
   }
 
   let d = [10000, 2000, 1000, 500, 100, 25, 10, 5, 1];
+  let change = [];
+  cid = cid.reverse();
 
-  let change = cid
-    .reverse()
-    .reduce((acc, e, i) => {
-      let c = Math.min(e[1]*100, Math.floor(diff / d[i]) * d[i]);
-      // ^ instead of using a while loop to calc how many would be taken from each category
-      // we either take the max amount in the category or the amount we need from the category
+  for (let i = 0; i < cid.length; i++) {
+    let c = Math.min(
+      cid[i][1] * 100,
+      Math.floor(diff / d[i]) * d[i]
+    );
+      // Minimum between cash in drawer (e[1]) and the maximum possible amount that can actually be taken from that category
+      // If latter is minimum, then drawer must have enough to fund the maximum taken from that category
+      // (There's no need to change the amount remaining in that category as we won't relook at that category)
+      // If former is minimum, then drawer must not have the maximum amount possible, and will take whatever it can give
 
       if (c > 0) {
         diff -= c;
-        acc.push([e[0], c/100]);
+        change.push([cid[i][0], c/100]);
       }
-
-      return acc;
-    }, []);
+      // takes difference away
+      // and adds what was just taken from the counter to the change list
+  }
 
   if (diff > 0) {
-    return { status: "INSUFFICIENT_FUNDS", change:[] }
+    return { status: "INSUFFICIENT_FUNDS", change: [] }
   } else {
     return { status: "OPEN", change }
   }
--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,16 +1,33 @@
 function convertToRoman(num) {
-  var rn = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-  var d = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];//an array of decimals values that match the index of the roman numerals
+  var r = {
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
+    "IX": 9,
+    "V": 5,
+    "IV": 4,
+    "I": 1
+  };
+
   var ans = "";
 
-  for (let i = 0; i < d.length; i++) {
-    while (d[i] <= num) {
-      num -= d[i];
-      ans += rn[i]
-    }
-  }
+  // using ([k, v]) instead of (k, v), we use destructuring
+  // to get the key and value
+  Object.entries(r).forEach(function([sym, number]) {
+    let rep = Math.floor(num/number);
+    ans += sym.repeat(rep);
+    num -= number * rep;
+  });
+  // this works as the only time we have to place a smaller number before a larger one, is one before a symbol; but as we have put this in the dictionary of roman numerals, we do not have to account for this
 
   return ans;
+
 }
 
 convertToRoman(16);
